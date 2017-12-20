8fa27156bc5aca303b3912e0eb27e3127236a57a
victor nieves sanchez <victor@slimbook.slimbook>
2017-12-20 14:50:11 +0100

    All files

---
+++ Space-Shooter-v2/Assets/Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour {
+    public Vector2 startWait, maneuverTime, maneuverWait;
+    public float dodge, smoothing, tilt;
+    public Boundary boundary;
+
+    private float targetManeuver, currentSpeed;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+	}
+
+    IEnumerator Evade() {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while (true) {
+            targetManeuver = Random.Range(1, dodge * -Mathf.Sign(transform.position.x));
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+
+	void Update () {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3(newManeuver, 0, currentSpeed);
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 0, Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+        rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x * -tilt);
+	}
+}
---
+++ Space-Shooter-v2/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    private AudioSource au;
+
+    //spacecraft movement
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    //bolt movement
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    private void Update(){
+        if (Input.GetButton("Fire1") && Time.time > nextFire){
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            au.Play();
+            //Other way:
+            //GameObject clone = Instantiate (shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+        }
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+        rb.velocity = movement * speed;
+        rb.position = new Vector3((Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax)), 0, (Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)));
+        rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x * -tilt);
+    }
+
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+        au = GetComponent<AudioSource>();
+    }
+}
